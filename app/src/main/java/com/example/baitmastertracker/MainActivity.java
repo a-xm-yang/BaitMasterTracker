44375eb8901038a0859cd31768f78b91068df8e8
NSeniow <Nicholas@seniow.com>
2019-11-23 13:21:58 -0500

    Start for loading images

--- app/src/main/java/com/example/baitmastertracker/MainActivity.java
+++ app/src/main/java/com/example/baitmastertracker/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.baitmastertracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+    private DatabaseReference mDatabase;
+    private User user;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        image = (ImageView)findViewById(R.id.imageView);
+        Glide.with(this).load("").into(image);
+
+        login();
+
+
+    }
+
+    private void login(){
+        mAuth.signInWithEmailAndPassword("nicholas@seniow.com", "Abc$123")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                            System.out.println(mAuth.getUid());
+
+                            mDatabase.child("Peeps").child(mAuth.getUid()).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                    user = dataSnapshot.getValue(User.class);
+                                    System.out.println(mAuth.getUid());
+
+                                    StorageReference riversRef = mStorageRef.child(mAuth.getUid());
+
+
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+                    }
+                });
     }
 }
